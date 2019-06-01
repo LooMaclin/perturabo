1d4fa08b546f45fcf375090ab634bb7bd00c522c
loomaclin <loo.maclin@protonmail.com>
2019-06-02 00:23:47 +0300

    fix clip

--- src/draw.rs
+++ src/draw.rs
@@ -23,22 +23,18 @@ impl<'a> Draw<'a> {
         });
     }
     pub fn point(&mut self, x: u32, y: u32, color: Color) {
-        if let Some(draw_rect) = self.draw_rect {
-            let y_offset = (draw_rect.y as u32 + y) * (draw_rect.w * 4);
-            let x_offset = (draw_rect.x as u32 + x) * 4;
-            if x_offset < draw_rect.x as u32 + draw_rect.w as u32
-                && y_offset < draw_rect.y as u32 + draw_rect.h as u32
-            {
-                let position = (x_offset + y_offset) as usize;
-                let (r, g, b, a) = color.to_rgba();
-                self.buff[position] = b;
-                self.buff[position + 1] = g;
-                self.buff[position + 2] = r;
-                self.buff[position + 3] = a;
-            }
-        } else {
-            let y_offset = y * (self.width * 4);
-            let x_offset = x * 4;
+        let y_offset = y * (self.width * 4);
+        let x_offset = x * 4;
+        let visible = self
+            .draw_rect
+            .map(|draw_rect| {
+                x_offset > draw_rect.x as u32
+                    && y_offset > draw_rect.y as u32
+                    && x_offset < draw_rect.x as u32 + draw_rect.w as u32
+                    && y_offset < draw_rect.y as u32 + draw_rect.h as u32
+            })
+            .unwrap_or(true);
+        if visible {
             let position = (x_offset + y_offset) as usize;
             let (r, g, b, a) = color.to_rgba();
             self.buff[position] = b;
